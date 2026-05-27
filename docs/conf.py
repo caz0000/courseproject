fffbd33d4c7130860bfd9656123fa087cd58454f
Cheyma Azzouz <127107699+caz0000@users.noreply.github.com>
2026-05-27 10:40:48 +0200

    Add Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = "courseproject-caz"
+copyright = "2026, Authors"
+author = "caz"
+release = "0.1"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+extensions = [
+    "myst_parser",  # in order to use markdown
+]
+
+myst_enable_extensions = [
+    "colon_fence",  # ::: can be used instead of ``` for better rendering
+]
+
+html_theme = "sphinx_rtd_theme"
